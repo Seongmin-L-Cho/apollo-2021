3fe54f48f300cfca777a1621ed4fb1084d0f1d94
DESKTOP-NJGRTB8\giner <Seongmin.L.Cho@gmail.com>
2022-04-24 21:49:47 +0900

    cache and style

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
 function App() {
-  return <div className="App"></div>;
+  console.log("fuck");
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
